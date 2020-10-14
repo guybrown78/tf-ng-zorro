c40c2cd1934d529a06ed21a371371a46cd40a2cc
Guy Brown <guy@digitalopolis.co.uk>
2020-10-14 13:17:03 +0100

    updated components for v10

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -17,6 +17,20 @@
 /***************************************************************************************************
  * BROWSER POLYFILLS
  */
+import 'core-js';
+
+// fixes 'forEach' issues on nodeList 
+// https://gist.github.com/bob-lee/e7520bfcdac266e5490f40c2759cc955
+if ('NodeList' in window && !NodeList.prototype.forEach) {
+	console.info('polyfill for IE11');
+	NodeList.prototype.forEach = function (callback, thisArg) {
+		 thisArg = thisArg || window;
+		 for (var i = 0; i < this.length; i++) {
+				callback.call(thisArg, this[i], i, this);
+		 }
+	};
+}
+
 
 /** IE10 and IE11 requires the following for NgClass support on SVG elements */
 import 'classlist.js';  // Run `npm install --save classlist.js`.
